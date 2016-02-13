06a3b837f07789528d4b1ee195f25d043daa1573
Sven Michael Klose <pixel@copei.de>
2016-02-13 22:01:14 +0100

    ENV-LOAD sucked in everything in the CL target. :(

--- environment/stage1/comparison-c.lisp
+++ environment/stage1/comparison-c.lisp
@@ -1,4 +1,4 @@
-; tré – Copyright (c) 2005–2006,2008–2009,2012–2015 Sven Michael Klose <pixel@copei.de>
+; tré – Copyright (c) 2005–2006,2008–2009,2012–2016 Sven Michael Klose <pixel@hugbox.org>
 
 (functional >= <= character>= character<= integer>= integer<= number>= number<=)
 
@@ -16,11 +16,11 @@
        (return nil))
     (setq n i)))
 
-(defun character>= (x y)
-  (| (character== x y) (character> x y)))
+(defun character>= (&rest x)
+  (apply #'>= (@ #'char-code x)))
 
-(defun character<= (x y)
-  (| (character== x y) (character< x y)))
+(defun character<= (&rest x)
+  (apply #'<= (@ #'char-code x)))
 
 (defun integer>= (x y)
   (| (integer== x y) (integer> x y)))
